bf7b422861b850971b8d308f3dd30bffa34225c3
itss0urav <sourav2021official@gmail.com>
2023-10-27 21:06:55 +0530

    updates

--- src/pages/BuyPage.jsx
+++ src/pages/BuyPage.jsx
@@ -7,27 +7,32 @@ const BuyPage = ({ product }) => {
     setQuantity(parseInt(event.target.value, 10));
   };
 
+  // The 10 here is the radix.
+  //It tells parseInt to convert the string into a decimal number.
+  // This is necessary because some numbers could be interpreted as octal (base 8) or hexadecimal (base 16) values.
+  //By specifying 10, we ensure that the value is always interpreted as a decimal number.
+
   const totalPrice = product.price * quantity;
 
-  const handleBuyNow = () => {
-    // Implement logic for handling the "Buy Now" action, e.g., adding the product to the cart, processing the purchase, etc.
-    // You can add your logic here.
-  };
+  const handleBuyNow = () => {};
 
-  const handleAddToCart = () => {
-    // Implement logic for handling the "Add to Cart" action, e.g., adding the product to the cart state.
-    // You can add your logic here.
-  };
+  const handleAddToCart = () => {};
 
   return (
     <div className="container mx-auto p-4">
       <div className="flex flex-col items-center mb-8">
         <h2 className="text-3xl font-semibold mb-4">{product.name}</h2>
-        <img className="w-1/2 object-cover mb-4" src={product.imageUrl} alt={product.name} />
+        <img
+          className="w-1/2 object-cover mb-4"
+          src={product.imageUrl}
+          alt={product.name}
+        />
         <p className="text-gray-600 mb-4">{product.description}</p>
       </div>
       <div className="mb-4 flex items-center">
-        <label className="mr-2 text-sm font-medium text-gray-600">Quantity:</label>
+        <label className="mr-2 text-sm font-medium text-gray-600">
+          Quantity:
+        </label>
         <input
           type="number"
           min="1"
@@ -36,7 +41,9 @@ const BuyPage = ({ product }) => {
           onChange={handleQuantityChange}
         />
       </div>
-      <h3 className="text-lg font-semibold mb-2">Total Price: ${totalPrice.toFixed(2)}</h3>
+      <h3 className="text-lg font-semibold mb-2">
+        Total Price: ${totalPrice.toFixed(2)}
+      </h3>
       <div className="flex items-center space-x-4">
         <button
           onClick={handleBuyNow}
