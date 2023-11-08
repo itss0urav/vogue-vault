0b37765d70fb63c123261a50cb468ef9a6e6c3c1
itss0urav <sourav2021official@gmail.com>
2023-11-08 14:13:35 +0530

    updates

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import React, { useContext } from "react";
 import Context from "../context/Context";
 import Navbar from "./Navbar";
-
+import Carousel from "../components/Carousel";
 const Cart = () => {
   const { cart, setCart } = useContext(Context);
 
@@ -43,51 +43,63 @@ const Cart = () => {
       <Navbar />
       <div className="container mx-auto p-4">
         <div className="text-center text-2xl font-bold">Cart</div>
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-bold">
-            Total Price: ₹{totalPrice.toFixed(2)}
-          </h2>
-          <button
-            onClick={handleBuyNow}
-            className="bg-yellow-400 text-black px-6 py-2 rounded hover:bg-yellow-600 focus:outline-none"
-          >
-            Proceed to Checkout
-          </button>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-          {cart.map((item, index) => (
-            <div key={item.id} className="bg-white p-4 rounded shadow-lg">
-              <img
-                className="object-cover mb-2"
-                src={item.imageUrl}
-                alt={item.name}
-              />
-              <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
-              <div className="text-gray-700">
-                <p className="mb-1">₹{item.price}</p>
-                <p className="mb-1">{item.description}</p>
-              </div>
-              <div className="mb-4 flex items-center">
-                <label className="mr-2 text-sm font-medium text-gray-600">
-                  Quantity:
-                </label>
-                <input
-                  type="number"
-                  min="1"
-                  className="w-16 h-10 border rounded px-3"
-                  value={item.quantity}
-                  onChange={(event) => handleQuantityChange(item.id, event)}
-                />
-              </div>
+        {cart.length === 0 ? (
+          <div className="flex flex-col items-center justify-center space-y-4">
+            <h2 className="mt-2 text-gray-500 text-xl ">Your cart is empty</h2>
+            <p className="text-gray-600">Check out fast selling products!</p>
+            <div className="w-full max-w-md">
+              <Carousel />
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className="flex justify-between items-center mb-6">
+              <h2 className="text-2xl font-bold">
+                Total Price: ₹{totalPrice.toFixed(2)}
+              </h2>
               <button
-                className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-200"
-                onClick={() => handleRemove(item.id)}
+                onClick={handleBuyNow}
+                className="bg-yellow-400 text-black px-6 py-2 rounded hover:bg-yellow-600 focus:outline-none"
               >
-                Remove
+                Proceed to Checkout
               </button>
             </div>
-          ))}
-        </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+              {cart.map((item, index) => (
+                <div key={item.id} className="bg-white p-4 rounded shadow-lg">
+                  <img
+                    className="object-cover mb-2"
+                    src={item.imageUrl}
+                    alt={item.name}
+                  />
+                  <h3 className="text-lg font-semibold mb-2">{item.name}</h3>
+                  <div className="text-gray-700">
+                    <p className="mb-1">₹{item.price}</p>
+                    <p className="mb-1">{item.description}</p>
+                  </div>
+                  <div className="mb-4 flex items-center">
+                    <label className="mr-2 text-sm font-medium text-gray-600">
+                      Quantity:
+                    </label>
+                    <input
+                      type="number"
+                      min="1"
+                      className="w-16 h-10 border rounded px-3"
+                      value={item.quantity}
+                      onChange={(event) => handleQuantityChange(item.id, event)}
+                    />
+                  </div>
+                  <button
+                    className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-200"
+                    onClick={() => handleRemove(item.id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import React, { useContext } from "react";
 import Context from "../context/Context";
 import Navbar from "./Navbar";
+import Carousel from "../components/Carousel";
 
 const Wishlist = () => {
   const { wishlist, setWishlist } = useContext(Context);
@@ -38,51 +39,65 @@ const Wishlist = () => {
       <Navbar />
       <div className="container mx-auto p-4">
         <div className="text-center text-2xl font-bold">Wishlist</div>
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-bold">
-            Total Price: ₹{totalPrice.toFixed(2)}
-          </h2>
-          <button
-            onClick={handleBuyNow}
-            className="bg-yellow-400 text-black px-6 py-2 rounded hover:bg-yellow-600 focus:outline-none"
-          >
-            Proceed to Checkout
-          </button>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-          {wishlist.map((data, index) => (
-            <div key={data.id} className="bg-white p-4 rounded shadow-lg">
-              <img
-                className="object-cover mb-2"
-                src={data.imageUrl}
-                alt={data.name}
-              />
-              <h3 className="text-lg font-semibold mb-2">{data.name}</h3>
-              <div className="text-gray-700">
-                <p className="mb-1">${data.price}</p>
-                <p className="mb-1">{data.description}</p>
-              </div>
-              <div className="mb-4 flex items-center">
-                <label className="mr-2 text-sm font-medium text-gray-600">
-                  Quantity:
-                </label>
-                <input
-                  type="number"
-                  min="1"
-                  className="w-16 h-10 border rounded px-3"
-                  value={data.quantity}
-                  onChange={(event) => handleQuantityChange(data.id, event)}
-                />
-              </div>
+        {wishlist.length === 0 ? (
+          <div className="flex flex-col items-center justify-center space-y-4">
+            <h2 className="mt-2 text-gray-500 text-xl ">
+              Your Wishlist is empty
+            </h2>
+            <p className="text-gray-600">Check out fast selling products!</p>
+            <div className="w-full max-w-md">
+              <Carousel />
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className="flex justify-between items-center mb-6">
+              <h2 className="text-2xl font-bold">
+                Total Price: ₹{totalPrice.toFixed(2)}
+              </h2>
               <button
-                className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-200"
-                onClick={() => handleRemove(data.id)}
+                onClick={handleBuyNow}
+                className="bg-yellow-400 text-black px-6 py-2 rounded hover:bg-yellow-600 focus:outline-none"
               >
-                Remove
+                Proceed to Checkout
               </button>
             </div>
-          ))}
-        </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+              {wishlist.map((data, index) => (
+                <div key={data.id} className="bg-white p-4 rounded shadow-lg">
+                  <img
+                    className="object-cover mb-2"
+                    src={data.imageUrl}
+                    alt={data.name}
+                  />
+                  <h3 className="text-lg font-semibold mb-2">{data.name}</h3>
+                  <div className="text-gray-700">
+                    <p className="mb-1">${data.price}</p>
+                    <p className="mb-1">{data.description}</p>
+                  </div>
+                  <div className="mb-4 flex items-center">
+                    <label className="mr-2 text-sm font-medium text-gray-600">
+                      Quantity:
+                    </label>
+                    <input
+                      type="number"
+                      min="1"
+                      className="w-16 h-10 border rounded px-3"
+                      value={data.quantity}
+                      onChange={(event) => handleQuantityChange(data.id, event)}
+                    />
+                  </div>
+                  <button
+                    className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition duration-200"
+                    onClick={() => handleRemove(data.id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
