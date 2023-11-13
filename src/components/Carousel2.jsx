eb80cbc76b1f9b5b1784e83377d5be9213e404f6
itss0urav <sourav2021official@gmail.com>
2023-11-13 17:11:03 +0530

    after admin update

--- src/components/Carousel2.jsx
+++ src/components/Carousel2.jsx
@@ -3,10 +3,11 @@ import image1 from "../assets/1.png";
 import image2 from "../assets/2.png";
 import image3 from "../assets/3.png";
 import image4 from "../assets/4.png";
+import image5 from "../assets/5.png";
 // import Carousel from "./Carousel";
 
 const Carousel2 = () => {
-  const images = [image1, image2, image3, image4];
+  const images = [image1, image2, image3, image4, image5];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isFading, setIsFading] = useState(false);
 
@@ -14,7 +15,7 @@ const Carousel2 = () => {
     const timer = setInterval(() => {
       setIsFading(true);
       setTimeout(() => {
-        setCurrentImageIndex((currentImageIndex + 1) % 4);
+        setCurrentImageIndex((currentImageIndex + 1) % 5);
         setIsFading(false);
       }, 1000);
     }, 4000);
