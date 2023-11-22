41f9f12eaef0362c864824da7dedfd2664e0936f
itss0urav <sourav2021official@gmail.com>
2023-11-22 21:31:12 +0530

    updates

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -6,6 +6,7 @@ const TosPage = () => {
     <>
       <Navbar />
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        
         <h1 className="text-3xl font-semibold text-center mb-8">
           Terms of Service
         </h1>
